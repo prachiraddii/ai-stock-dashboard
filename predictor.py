0904ef99f7151a7a89d123ea1c74706c6638e339
Prachi Pandurang Raddi <prachiraddi14@gmail.com>
2026-03-14 00:52:09 +0530

    Add forecast_prices function to predict future prices
    Added a new function to forecast future prices based on random changes.

--- predictor.py
+++ predictor.py
@@ -1,9 +1,25 @@
 import random
+import pandas as pd
 
 def predict_price(current_price):
 
-    change = random.uniform(-2, 2)
-
+    change = random.uniform(-2,2)
     predicted = current_price + change
 
     return round(predicted,2)
+
+
+def forecast_prices(data):
+
+    last_price = data['Close'].iloc[-1]
+
+    future_prices = []
+
+    for i in range(10):
+
+        change = random.uniform(-3,3)
+        last_price = last_price + change
+
+        future_prices.append(last_price)
+
+    return future_prices
